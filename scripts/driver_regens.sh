c06bc03eaf2b02272ffca42443bd38e1b7c88d5f
valcap <valcap74@gmail.com>
2015-03-13 11:55:18 +0000

    Minor changes

--- scripts/driver_regens.sh
+++ scripts/driver_regens.sh
@@ -6,27 +6,28 @@
 #
 # Author  : Valerio Capecchi <capecchi@lamma.rete.toscana.it>
 # Date    : 2013-05-13
-# UpDate  : 2013-07-30; 2014-12-22; 2015-03-09
+# UpDate  : 2013-07-30; 2014-12-22; 2015-03-13
 # Purpose : GEFS driver for regional ensemble
 #
 ###########################################################################
 
 # argument enviroment file
-if [ $# -ne 1 ] ; then
- echo "Usage: sh ./$0 envfile"
- echo "Example: sh ./$0 ../env/gefs.env"
+if [ $# -ne 2 ] ; then
+ echo "Usage: sh ./$0 envfile run_hour"
+ echo "Example: sh ./$0 ../env/gefs.env run_hour"
  echo ""
  exit 1
 fi
 envfile=$1
+run=$2
 
 # Log file
-if [ ! -d "../log" ]; then
-  exec 1>>`basename $0 .sh`_`date +"%Y%m%d"`.log
-else
-  exec 1>>../log/`basename $0 .sh`_`date +"%Y%m%d"`.log
-  exec 2>&1
-fi
+#if [ ! -d "../log" ]; then
+#  exec 1>>`basename $0 .sh`_`date +"%Y%m%d"`.log
+#else
+#  exec 1>>../log/`basename $0 .sh`_`date +"%Y%m%d"`.log
+#  exec 2>&1
+#fi
 
 # Functions
 function notice {
@@ -43,26 +44,30 @@ else
 fi
 
 # year, month, day, hour, lenght of forecast
-initdate=`date +%Y --date now``date +%m --date now``date +%d --date now`
-cur_hour=`date +%H --date now`
-if [ $cur_hour -eq 18 ]; then
-  run=12
-fi
-if [ $cur_hour -eq 06 ]; then
-  run=00
-fi
-if [ "$run" != '00' ] && [ "$run" != '12' ] ; then
-  notice "run $run is not matching 12 or 00..."
-#  notice "Exiting..."; exit 1;
-  notice "anyway.....run=00 by default"
-  run=00
-fi
+case $run in
+00)
+  initdate=`date +%Y --date now``date +%m --date now``date +%d --date now`
+  ;;
+06)
+  initdate=`date +%Y --date now``date +%m --date now``date +%d --date now`
+  ;;
+12)
+  initdate=`date +%Y --date now``date +%m --date now``date +%d --date now`
+  ;;
+18)
+  initdate=`date +%Y --date "now - 1 day"``date +%m --date "now - 1 day"``date +%d --date "now - 1 day"`
+  ;;
+*)
+  notice "run $run is not matching 00 or 06 or 12 or 18..exiting.."; exit 1
+  ;;
+esac
+
 lenght=$run_lenght # getting the lenght from the env file
 
 ##########################################
-notice "+++ +++ +++"
 notice "+++START OF $0 $1 $initdate $run $lenght"
 initime=`date +%s`
+exit 1
 ##########################################
 
 cd $SCRPDIR
